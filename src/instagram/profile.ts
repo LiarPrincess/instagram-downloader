2a60e7110277a1cd6f846c4f1e91fb65da37bc27
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-11-23 10:16:20 +0100

    Cache profile

--- src/instagram/profile.ts
+++ src/instagram/profile.ts
@@ -1,7 +1,8 @@
-import { get } from './common/request';
-import { Urls } from './common/constants';
+import { getJSON } from './common/request';
 import { GuestAuthentication } from './authenticate';
 import { ProfileResponse } from './response-types/profile-response';
+
+import * as cache from '../cache';
 
 export interface Profile {
   readonly id: string;
@@ -20,9 +21,12 @@ export interface Profile {
   readonly postCount: number;
 }
 
-export async function getProfile(auth: GuestAuthentication, username: string): Promise<Profile> {
-  const url = `https://www.instagram.com/${username}/?__a=1`;
-  const response = await get(auth, url) as ProfileResponse;
+export async function getProfile(
+  auth: GuestAuthentication,
+  username: string,
+  useCache: boolean
+): Promise<Profile> {
+  const response = await get(auth, username, useCache);
   const user = response.graphql.user;
 
   return {
@@ -41,4 +45,26 @@ export async function getProfile(auth: GuestAuthentication, username: string): P
     followingCount: user.edge_follow.count,
     postCount: user.edge_owner_to_timeline_media.count
   };
+}
+
+async function get(
+  auth: GuestAuthentication,
+  username: string,
+  useCache: boolean
+): Promise<ProfileResponse> {
+  const cacheKey = `${username}_profile.json`;
+
+  if (useCache) {
+    const string = await cache.get(cacheKey);
+    if (string) {
+      const result = JSON.parse(string);
+      return result;
+    }
+  }
+
+  const url = `https://www.instagram.com/${username}/?__a=1`;
+  const response = await getJSON(auth, url);
+  await cache.put(cacheKey, JSON.stringify(response));
+
+  return response;
 }
